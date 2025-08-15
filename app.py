6e2b45f58b9750bc85406516da82612b5a72b30f
joel172003@gmail.com <Joel172003@gmail.com>
2025-08-15 14:05:15 +0530

    added logic to the app.py post and get

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+       
+        if 'plant_image' in request.files:
+            uploaded_file = request.files['plant_image']
+            
+            
+            if uploaded_file.filename != '':
+    
+                base_dir = os.path.abspath(os.path.dirname(__file__))
+                uploads_dir = os.path.join(base_dir, 'static/uploads')
+                os.makedirs(uploads_dir, exist_ok=True) # Ensure the folder exists
+
+                
+                file_path = os.path.join(uploads_dir, uploaded_file.filename)
+                uploaded_file.save(file_path)
+
+                
+                print(f"File saved successfully to: {file_path}")
+
+    # This runs on a GET request
     return render_template('index.html')
 
 if __name__ == '__main__':
